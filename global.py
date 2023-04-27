c4358789ca1bacca4594af722462028061ad0b16
gusdnr <20221022@hanbat.ac.kr>
2023-04-27 17:46:10 +0900

    add function examples

---
+++ global.py
@@ -0,0 +1,14 @@
+#두 정수를 더한 값을 반환하는 함수 add_numbers를 작성하시오.
+# global variable
+result=0
+def add_numbers(num1, num2):
+    global result
+    result = num1 + num2
+    return result
+
+input1 = 30
+input2 = 40
+add_result = add_numbers(input1, input2)
+
+print(f"{input1} + {input2} = {add_result}")
+print(f"{input1} + {input2} = {result}")
